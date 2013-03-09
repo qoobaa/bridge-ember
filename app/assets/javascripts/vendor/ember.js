4548e5a89ec63fff17744c030298f0cff956176d
Kuba Kuźma <kuba@jah.pl>
2013-03-09 20:40:39 +0100

    removed version logging from ember

--- app/assets/javascripts/vendor/ember.js
+++ app/assets/javascripts/vendor/ember.js
@@ -25035,11 +25035,11 @@ var Application = Ember.Application = Ember.Namespace.extend({
     this.deferUntilDOMReady();
     this.scheduleInitialize();
 
-    Ember.debug('-------------------------------');
-    Ember.debug('Ember.VERSION : ' + Ember.VERSION);
-    Ember.debug('Handlebars.VERSION : ' + Ember.Handlebars.VERSION);
-    Ember.debug('jQuery.VERSION : ' + Ember.$().jquery);
-    Ember.debug('-------------------------------');
+    // Ember.debug('-------------------------------');
+    // Ember.debug('Ember.VERSION : ' + Ember.VERSION);
+    // Ember.debug('Handlebars.VERSION : ' + Ember.Handlebars.VERSION);
+    // Ember.debug('jQuery.VERSION : ' + Ember.$().jquery);
+    // Ember.debug('-------------------------------');
   },
 
   /**
