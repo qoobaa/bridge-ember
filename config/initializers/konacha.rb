f3e551af90b049f5c0fe29bb3368dd6ea2fd14d2
Wojciech Wnętrzak <w.wnetrzak@gmail.com>
2013-03-11 10:30:11 +0100

    updated to fixed version of konacha

--- config/initializers/konacha.rb
+++ config/initializers/konacha.rb
@@ -1,8 +1,4 @@
 if defined?(Konacha)
-  # Fake option for konacha
-  # https://github.com/jfirebaugh/konacha/issues/120
-  Rails.application.config.assets.enabled = true
-
   require "capybara/poltergeist"
 
   Konacha.configure do |config|
