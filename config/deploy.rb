b6bdd23fe5795306886cc484ce7b1bc0f810a507
Wojciech Wnętrzak <w.wnetrzak@gmail.com>
2013-04-01 00:46:17 +0200

    back to original repo

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@ require "bundler/capistrano"
 set :application, "bridge"
 set :deploy_to, "/home/bridge/apps/#{application}"
 set :user, "bridge"
-set :repository,  "git://github.com/qoobaa/bridge-ember-zomg.git"
+set :repository,  "git://github.com/qoobaa/bridge-ember.git"
 set :use_sudo, false
 set :default_environment, "PATH" => "/home/bridge/.nvm/v0.10.2/bin:/home/bridge/.rbenv/shims:/home/bridge/.rbenv/bin:$PATH"
 set :rails_env, "production"
