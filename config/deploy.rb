b320c781941dad5f30307273f68f1411aa4b74a9
Kuba Kuźma <kuba@jah.pl>
2013-03-31 16:25:55 +0200

    regenerated gemfile.lock

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,4 +7,6 @@ set :repository,  "git://github.com/qoobaa/bridge-ember.git"
 set :use_sudo, false
 set :default_environment, "PATH" => "/home/bridge/.nvm/v0.10.2/bin:/home/bridge/.rbenv/shims:/home/bridge/.rbenv/bin:$PATH"
 
+set :shared_children, shared_children + %w[config/database.yml]
+
 server "bridge.jah.pl:43377", :web, :app, :db, primary: true
